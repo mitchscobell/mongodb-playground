36f07ccf9bf251080e7e2af32350baf8f9cea115
Mitch Scobell <mitchscobell@gmail.com>
2023-01-06 17:31:11 -0600

    seeds the database and finds a movie

--- node_quickstart/index.js
+++ node_quickstart/index.js
@@ -1,27 +1,65 @@
 const { MongoClient } = require("mongodb");
 
 // Replace the uri string with your connection string.
-const username = "";
-const password = "";
-const clusterUrl = "";
+const username = "myUserAdmin";
+const password = "password";
+const clusterUrl = "127.0.0.1";
+const portNumber = "27017";
+const retryWrites = true;
 const uri =
-    `mongodb+srv://${username}:${password}@${clusterUrl}?retryWrites=true&w=majority`;
-
+    `mongodb://${username}:${password}@${clusterUrl}:${portNumber}?retryWrites=${retryWrites}&w=majority`;
 const client = new MongoClient(uri);
 
 async function run() {
+
     try {
+
+        await seedDatabase();
+
         const database = client.db('sample_mflix');
         const movies = database.collection('movies');
 
         // Query for a movie that has the title 'Back to the Future'
         const query = { title: 'Back to the Future' };
-        const movie = await movies.findOne(query);
+        const foundMovie = await movies.findOne(query);
 
-        console.log(movie);
+        console.log(foundMovie);
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
+async function seedDatabase() {
+
+    const movie1 =
+    {
+        title: 'Back to the Future',
+        description: 'Marty McFly, a 17-year-old high school student, is accidentally sent 30 years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.'
+    }
+
+    const listOfMovies = [
+        {
+            title: 'Back to the Future Part III',
+            description: 'Stranded in 1955, Marty McFly learns about the death of Doc Brown in 1885 and must travel back in time to save him. With no fuel readily available for the DeLorean, the two must figure how to escape the Old West before Emmett is murdered.'
+        },
+        {
+            title: 'Back to the Future Part II',
+            description: 'After visiting 2015, Marty McFly must repeat his visit to 1955 to prevent disastrous changes to 1985...without interfering with his first trip.'
+        }
+    ]
+
+    const database = client.db('sample_mflix');
+    const collections = await (await database.listCollections().toArray()).map(collection => collection.name);
+
+    if (collections.includes("movies")) {
+        console.log('Dropping database: ' + await database.collection('movies').drop());
+        await database.createCollection('movies');
+    }
+
+    const movies = database.collection('movies');
+
+    movies.insertOne(movie1);
+    movies.insertMany(listOfMovies);
+}
+
 run().catch(console.dir);
